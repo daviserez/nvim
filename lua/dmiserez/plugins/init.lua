5eb2b4ba09c25f9646437c7cd9e531a94b3e63f8
David M <davidmiserez@gmail.com>
2025-05-12 09:22:50 +0200

    Add shortcuts for managing terminal

--- lua/dmiserez/plugins/init.lua
+++ lua/dmiserez/plugins/init.lua
@@ -1,3 +1,8 @@
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Quit terminal mode" })
+
+-- botright to full width
+vim.keymap.set('n', '<leader>tt', ':belowright 20split | terminal<CR>', { noremap = true, silent = true, desc = "Open terminal below" })
+
 return {
   "nvim-lua/plenary.nvim",
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
